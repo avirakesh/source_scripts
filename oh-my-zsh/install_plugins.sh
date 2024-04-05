cd706772fd72fcbb1f7ddffc4f0bde49cef3e220
Avichal Rakesh <avichal.rakesh@gmail.com>
2024-04-04 20:08:56 -0700

    add script to install zsh plugins

---
+++ oh-my-zsh/install_plugins.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+echo "Installing zsh-autosuggestions"
+echo "------------------------------"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo ""
+echo "Installing zsh-syntax-highlighting"
+echo "----------------------------------"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
